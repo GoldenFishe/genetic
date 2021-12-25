8b198f9ca363c48776bf02d6134a1b220d3f2227
Anton Ryazanov <a.g.ryazanov@tinkoff.ru>
2021-12-25 17:52:55 +0300

    +fix

--- src/getData.js
+++ src/getData.js
@@ -1,46 +1,77 @@
 const fs = require("fs/promises");
 const tf = require("@tensorflow/tfjs");
 
-async function getFilenames() {
+const SHUFFLE = 10;
+
+async function getFilenames(type, dataType) {
   try {
-    return fs.readdir("./data/test/paper");
+    return fs.readdir(`${__dirname}/data/${dataType}/${type}`);
   } catch (err) {
     console.log(err);
   }
 }
 
-async function getFile(filename) {
+async function getFile(type, filename, dataType) {
   try {
-    const file = await fs.readFile(`./data/test/paper/${filename}`);
-    const pixelData = { width: 300, height: 300, data: file };
+    const file = await fs.readFile(`${__dirname}/data/${dataType}/${type}/${filename}`);
+    const pixelData = {width: 300, height: 300, data: file};
     return tf.browser.fromPixels(pixelData);
   } catch (err) {
     console.error(err);
   }
 }
 
-async function getData() {
-  const filenames = await getFilenames();
+async function getData(type, dataType) {
+  const filenames = await getFilenames(type, dataType);
   const data = [];
   for (let filename of filenames) {
-    const file = await getFile(filename);
+    const file = await getFile(type, filename, dataType);
     data.push(file);
   }
   return tf.data.array(data);
 }
 
-async function getLabels(size) {
+async function getLabels(size, label) {
   const labels = [];
   for (let i = 0; i < size; i++) {
-    labels.push(tf.tensor(1));
+    labels.push(tf.tensor(label));
   }
   return tf.data.array(labels);
 }
 
-async function getDataset() {
-  const data = await getData();
-  const labels = await getLabels(data.size);
-  return tf.data.zip({ xs: data, ys: labels }).batch(3)
+async function getTrainDataset() {
+  const paperData = await getData('paper', 'train');
+  const rockData = await getData('rock', 'train');
+  const scissorsData = await getData('scissors', 'train');
+
+  const paperLabels = await getLabels(paperData.size, 1);
+  const rockLabels = await getLabels(rockData.size, 2);
+  const scissorsLabels = await getLabels(scissorsData.size, 3);
+
+  const data = paperData.concatenate(rockData).concatenate(scissorsData).map(item => {
+    return item.div(255);
+  });
+  const labels = paperLabels.concatenate(rockLabels).concatenate(scissorsLabels);
+
+  return tf.data.zip({xs: data, ys: labels}).batch(3);
 }
 
-module.exports = getDataset;
+async function getTestDataset() {
+  const paperData = await getData('paper', 'test');
+  const rockData = await getData('rock', 'test');
+  const scissorsData = await getData('scissors', 'test');
+
+  const paperLabels = await getLabels(paperData.size, 1);
+  const rockLabels = await getLabels(rockData.size, 2);
+  const scissorsLabels = await getLabels(scissorsData.size, 3);
+
+  const data = paperData.concatenate(rockData).concatenate(scissorsData);
+  const labels = paperLabels.concatenate(rockLabels).concatenate(scissorsLabels);
+
+  return tf.data.zip({xs: data, ys: labels}).batch(3)
+}
+
+module.exports = {
+  getTrainDataset,
+  getTestDataset
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const tf = require("@tensorflow/tfjs-node")
+const tf = require("@tensorflow/tfjs-node");
 
-const getDataset = require("./getData");
+const { getTrainDataset, getTestDataset } = require("./getData");
 
 const IMAGE_WIDTH = 300;
 const IMAGE_HEIGHT = 300;
@@ -9,37 +9,29 @@ const NUM_OUTPUT_CLASSES = 3;
 const CLASSES = ["paper", "rock", "scissors"];
 
 function getModel() {
-  const model = tf.sequential();
-  const inputLayer = tf.layers.conv2d({
-    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    kernelSize: 5,
-    filters: 8,
-    strides: 1,
-    activation: "relu",
-    kernelInitializer: "varianceScaling"
+  const model = tf.sequential({
+    layers: [
+      tf.layers.conv2d({
+        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        kernelSize: 64,
+        filters: 8,
+        strides: 1,
+        activation: "relu",
+        kernelInitializer: "varianceScaling"
+      }),
+      tf.layers.maxPool2d({ poolSize: [2, 2], strides: [2, 2] }),
+      tf.layers.conv2d({
+        kernelSize: 64,
+        filters: 8,
+        strides: 1,
+        activation: "relu",
+        kernelInitializer: "varianceScaling"
+      }),
+      tf.layers.maxPool2d({ poolSize: [2, 2], strides: [2, 2] }),
+      tf.layers.flatten(),
+      tf.layers.dense({ units: NUM_OUTPUT_CLASSES, kernelInitializer: "varianceScaling", activation: "softmax" })
+    ]
   });
-  const hiddenLayer1 = tf.layers.maxPool2d({ poolSize: [2, 2], strides: [2, 2] });
-  const hiddenLayer2 = tf.layers.conv2d({
-    kernelSize: 5,
-    filters: 16,
-    strides: 1,
-    activation: "relu",
-    kernelInitializer: "varianceScaling"
-  });
-  const hiddenLayer3 = tf.layers.maxPool2d({ poolSize: [2, 2], strides: [2, 2] });
-  const hiddenLayer4 = tf.layers.flatten();
-  const outputLayer = tf.layers.dense({
-    units: NUM_OUTPUT_CLASSES,
-    kernelInitializer: "varianceScaling",
-    activation: "softmax"
-  });
-
-  model.add(inputLayer);
-  model.add(hiddenLayer1);
-  model.add(hiddenLayer2);
-  model.add(hiddenLayer3);
-  model.add(hiddenLayer4);
-  model.add(outputLayer);
 
   const optimizer = tf.train.adam();
   model.compile({
@@ -51,24 +43,23 @@ function getModel() {
   return model;
 }
 
-async function train(model, dataset) {
+async function train(model, trainDataset, testDataset) {
   console.log("start");
-  const history = await model.fitDataset(dataset, {
-    epochs: 10,
-    callbacks: {onEpochEnd: (epoch, logs) => console.log(logs.loss)}
-  })
+  const history = await model.fitDataset(trainDataset, {
+    epochs: 5,
+    validationData: testDataset,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => console.log(logs.loss)
+    }
+  });
   console.log(history);
   console.log("end");
 }
 
 async function main() {
-  await tf.setBackend('tensorflow')
-  console.log(tf.getBackend());
-  // const model = getModel();
-  // const dataset = await getDataset();
-  // await train(model, dataset)
+  const model = getModel();
+  const [trainDataset, testDataset] = await Promise.all([getTrainDataset(), getTestDataset()]);
+  await train(model, trainDataset, testDataset);
 }
+
 main();
-
-
-
